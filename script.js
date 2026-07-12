3feab28b0fc8514125015d58c6fb06df9d84e648
Coder6999 <bmwxm63@gmail.com>
2026-07-12 23:46:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -199,3 +199,18 @@ duration:250
 });
 
 });
+document.querySelectorAll(".featured-card").forEach(card=>{
+
+card.addEventListener("mouseenter",()=>{
+
+card.style.transform="translateY(-10px) scale(1.02)";
+
+});
+
+card.addEventListener("mouseleave",()=>{
+
+card.style.transform="translateY(0) scale(1)";
+
+});
+
+});
